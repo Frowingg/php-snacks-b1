f2b71393a9a2d3fc24a198affa671686d5c2ae3e
Frowingg <edbico0@gmail.com>
2022-07-24 17:38:12 +0200

    snack6

--- snack6.php
+++ snack6.php
@@ -1,3 +1,42 @@
+<?php 
+$students = [
+        '1A' => [
+                [
+                    'name' => 'Alessio',
+                    'lastname' => 'Rossi',
+                    'avarage_vote' => '4'
+                ],
+                [
+                    'name' => 'Marco',
+                    'lastname' => 'Verdi',
+                    'avarage_vote' => '8'
+                ],
+                [
+                    'name' => 'Giorgio',
+                    'lastname' => 'Violi',
+                    'avarage_vote' => '7'
+                ],
+        ],
+        '1B' => [
+            [
+                'name' => 'Giulio',
+                'lastname' => 'Gialli',
+                'avarage_vote' => '9'
+            ],
+            [
+                'name' => 'Riccardo',
+                'lastname' => 'Rossi',
+                'avarage_vote' => '5'
+            ],
+            [
+                'name' => 'Claudio',
+                'lastname' => 'Bianchi',
+                'avarage_vote' => '7'
+            ],
+        ],
+    ];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +46,25 @@
     <title>PHP</title>
 </head>
 <body>
+
+    <?php
+    foreach($students as $thisClass) {
+
+        foreach($thisClass as $thisStudent) {
+
+            foreach($thisStudent as $thisInfo => $thisInfoValue) {
+                echo $thisInfo . ' = ';                
+                echo $thisInfoValue . ' ';  
+                ?>
+                <br>
+                <?php
+            }
+            ?>
+            <br>
+            <?php
+        }
+    }
+    ?>
 
 </body>
 </html>
